b910c50fc50674499abe26762f4879a38baa55b2
Toño Aguirre <caaz160782@gmail.com>
2023-02-13 18:08:34 -0600

    ver tickets

--- public/js/nuevo-ticket.js
+++ public/js/nuevo-ticket.js
@@ -1,3 +1,31 @@
+//referencias html
+const lblNuevoTicket= document.querySelector('#lblNuevoTicket');
+const btnCrear= document.querySelector('button');
 
 
-console.log('Nuevo Ticket HTML');
+const socket = io();
+
+socket.on('connect', () => {
+    btnCrear.disabled=false;
+    
+    socket.emit( 'ultimo-ticket', null, ( ultimo ) => {
+        lblNuevoTicket.innerText = ultimo;
+        // console.log('Desde el server', ticket );
+    });
+});
+
+socket.on('disconnect', () => {
+    btnCrear.disabled=true;
+});
+
+
+
+btnCrear.addEventListener( 'click', () => {
+     socket.emit( 'siguiente-ticket', null, ( ticket ) => {
+        lblNuevoTicket.innerText = ticket;
+        // console.log('Desde el server', ticket );
+    });    
+
+});
+
+
